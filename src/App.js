7a87d6207bc0bbcd172c4499c2933463948959f3
Syed Talha <syedt.in00@gmail.com>
2024-03-25 23:43:43 +0500

    modefied App.js for route security

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./Context/AuthContext";
 
@@ -12,14 +12,22 @@ function App() {
   const {currentUser} = useContext(AuthContext)
   console.log(currentUser)
 
-  
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser) {
+      return <Navigate to="/"/>
+    }
+  }
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Register />} />        
-        <Route path="/home" element={<Home />} />        
+        <Route path="/home" element={
+        <ProtectedRoute>
+        <Home />
+        </ProtectedRoute>
+        } />        
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
